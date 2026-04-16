fc397d08a9ea62b14842a46daf275b2254517245
bakeunbi99 <peb7949@naver.com>
2026-04-16 10:20:59 +0900

    [수정] 페이지 이동/새로고침 시 로그인 상태 유지
    [수정] 페이지 이동/새로고침 시 로그인 상태 유지

--- frontend/chargerReservation/src/routers/AppRouter.tsx
+++ frontend/chargerReservation/src/routers/AppRouter.tsx
@@ -15,7 +15,6 @@ import BasicLayout from "../layout/basic/basicLayout";
 import Stations from "../pages/station/Stations";
 import ProtectedRoute from "../common/ProtectedRoute";
 import MyPage from "../pages/member/MyPage";
-import BasicLayout from "../layout/basic/basicLayout";
 
 export const AppRouter = () => {
   return (
@@ -27,7 +26,6 @@ export const AppRouter = () => {
         <Route element={<BasicLayout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/mypage" element={<MyPage />} />
-          <Route path="/stations" element={<Stations />} />
           <Route path="/reservation" element={<ReservationPage />} />
           <Route path="/reservations" element={<MyPage />} />
         </Route>
@@ -55,6 +53,10 @@ export const AppRouter = () => {
           <Route path="/admin/penalty" element={<AdminPenaltyPage />} />
           <Route path="/admin/inquiry" element={<AdminInquiryPage />} />
         </Route>
+        {/* ==========================================
+            충전소 찾기
+           ========================================== */}
+        <Route path="/stations" element={<Stations />} />
       </Routes>
     </BrowserRouter>
   );
